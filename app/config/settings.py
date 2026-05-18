e2a6dbc1a12b0b2b22ee3149858b9056db70f2f4
luischavez23 <lchavezdiazdesandi@gmail.com>
2026-05-18 17:58:38 -0600

    ALLOW ORIGINS CHANGE

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+FRONTEND_URL=os.getenv("FRONTEND_URL")
 RESEND_API_KEY = os.getenv("RESEND_API_KEY")
 EMAIL_TO=os.getenv("EMAIL_TO")
 FROM_EMAIL=os.getenv("FROM_EMAIL")
--- app/main.py
+++ app/main.py
@@ -3,12 +3,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.rsvp import router as rsvp_router
 
+from app.config.settings import FRONTEND_URL
+
+
 app = FastAPI()
 
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
