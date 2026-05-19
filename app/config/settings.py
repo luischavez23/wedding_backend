b83a63a1d7db7c6eccbd18e8f1313da9e6027b04
luischavez23 <lchavezdiazdesandi@gmail.com>
2026-05-18 21:30:29 -0600

    Add new contact email

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 FRONTEND_URL=os.getenv("FRONTEND_URL")
 RESEND_API_KEY = os.getenv("RESEND_API_KEY")
-EMAIL_TO=os.getenv("EMAIL_TO")
+EMAIL_TO=os.getenv("EMAIL_TO").split(",")
 FROM_EMAIL=os.getenv("FROM_EMAIL")
 HTTP_RESEND=os.getenv("HTTP_RESEND")
 ENV=os.getenv("ENV", "production")
